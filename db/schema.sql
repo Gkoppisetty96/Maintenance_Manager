c8c6853fef92d4852c58fa4ed961ec6160130398
tayler176 <47032349+tayler176@users.noreply.github.com>
2019-05-18 12:11:16 -0400

    My sql check (#8)
    * mysql2 added
    
    * checking the database and travis staus
    
    * checking the database and travis staus
    
    * checking the database and travis staus

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,17 @@
-DROP DATABASE IF EXISTS "databaseNameHere_db";
-CREATE DATABASE "databaseNameHere_db";
+DROP DATABASE IF EXISTS task_db;
+CREATE DATABASE task_db;
 
-USE "databaseNameHere_db";
+USE task_db;
 
-CREATE TABLE "tableNameHere"(
-    id INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY(id),
-    "columnNameHere" VARCHAR(50) NOT NULL,
-    -- the below is for when we have to deplay on heroku
-    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE tasks(
+	id INT NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY(id),
+    name VARCHAR(30) NOT NULL,
+    zone VARCHAR(20) NOT NULL,
+    unit_number VARCHAR(40),
+    problem VARCHAR(25) NOT NULL,
+	note TEXT(250) NOT NULL,
+	completed BOOLEAN DEFAULT false,
+    createdAt TIMESTAMP NOT NULL,
+    severity INT
 );
