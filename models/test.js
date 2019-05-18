5942d3894b2ddf8979017cdfb9e929b684e83ff9
Taylor <Taylor@Taylors-MacBook-Pro-2.local>
2019-05-18 12:30:07 -0400

    added the test.js

--- models/test.js
+++ models/test.js
@@ -1,7 +1,46 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+  var Example = sequelize.define("tasks", {
+    id:{
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    name: {
+      type: Sequelize.VARCHAR,
+      allowNull: false,
+      len: [1,30],
+    },
+    zone: {
+      type: Sequelize.VARCHAR,
+      allowNull: false,
+      len: [1,20],
+    },
+    unit_number:{
+      type: Sequelize.VARCHAR,
+      allowNull: false,
+      len: [1,40],
+    },
+    problem:{
+      type: Sequelize.VARCHAR,
+      allowNull: false,
+      len: [1,25],
+    },
+    note: {
+      type: Sequelize.TEXT,
+      allowNull: false,
+      len: [1,250],
+    },
+    completed: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      DEFAULTVALUE: false,
+    },
+    severity:{
+      type: Sequelize.INTEGER,
+      allowNull: true,
+      validate: { min: 1 , max: 10 } 
+    }
   });
   return Example;
 };
