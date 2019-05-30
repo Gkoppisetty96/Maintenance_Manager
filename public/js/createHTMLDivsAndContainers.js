6d3610dfb6c0d2be964e02346a57664fdf6385ff
Josh Eshleman <eshlemanjosh@gmail.com>
2019-05-29 20:04:55 -0400

    admin access, on enter key..etc

---
+++ public/js/createHTMLDivsAndContainers.js
@@ -0,0 +1,219 @@
+// Creating buttons
+var path = window.location.pathname;
+var page = path.split("/").pop();
+console.log(page);
+
+var htmlBaseDivsCreater = () => {
+    navBarCreater();
+    $("<div>").attr({
+        id: "everything-container",
+    }).appendTo("body")
+    $("<div>").attr({
+        class: "row text-center",
+    }).appendTo("#everything-container");
+    containerForListOfTasksForLoopCreater();
+    buttonForTestingCreater();
+    if (page === "index.html") {
+        taskSummaryAndBreadCrumbCreater();
+        newTaskButtonOnHTMLNoMySQLFunctionalityCreater();
+        cancelTaskButtonCreator();
+        // buttonContainerDivForLoopCreater();
+    } else if (page === "admin.html") {
+        droppableFunctionality();
+    }
+    footerCreater();
+};
+
+var navBarCreater = () => {
+    $("<nav>").attr({
+        class: "navbar navbar-expand-lg navbar-dark bg-dark"
+    }).appendTo("body");
+    $("<a>Task Manager</a>").attr({
+        class: "navbar-brand"
+    }).appendTo(".navbar");
+    $("<button>").attr({
+        type: "button",
+        class: "navbar-toggler",
+        "data-toggle": "collapse",
+        "ata-target": "#navbarNav",
+        "aria-controls": "navbarNav",
+        "aria-expanded": "false",
+        "aria-label": "Toggle navigation"
+    }).appendTo(".navbar");
+    $("<span>").attr({
+        class: "navbar-toggler-icon"
+    }).appendTo(".navbar-toggler");
+    $("<div>").attr({
+        id: "navbarNav",
+        class: "collapse navbar-collapse",
+    }).appendTo(".navbar");
+    $("<ul>").attr({
+        class: "navbar-nav",
+    }).appendTo("#navbarNav");
+    $("<li>").attr({
+        id: "nav-item-index",
+        class: "nav-item",
+    }).appendTo(".navbar-nav");
+    $("<a>Home</a>").attr({
+        class: "nav-link active",
+        href: "index.html"
+    }).appendTo("#nav-item-index");
+        $("<li>").attr({
+            id: "nav-item-admin",
+            class: "nav-item",
+        }).appendTo(".navbar-nav");
+        $("<a>Admin</a>").attr({
+            class: "nav-link active",
+        }).appendTo("#nav-item-admin");
+    }
+
+var containerForListOfTasksForLoopCreater = () => {
+    var taskStatusArray = ["", "open", "closed"];
+    for (var h = 1; h < taskStatusArray.length; h++) {
+        $("<div>").attr({
+            id: "container" + h + "Div",
+            class: "col-sm-6",
+        }).appendTo(".row");
+        $("<div>").attr({
+            id: "container" + h + "Card",
+            class: "card",
+        }).appendTo("#container" + h + "Div");
+        if (taskStatusArray[h] === "open") {
+            // console.log(taskStatusArray[h]);
+            $("<div>").attr({
+                class: "card-body " + taskStatusArray[h],
+            }).appendTo("#container" + h + "Card");
+            $("<h5>Pending Tasks</p>").attr({
+                class: "card-title",
+            }).appendTo("." + taskStatusArray[h]);
+            $("<p>These tasks have been recieved</p>").attr({
+                class: "card-text",
+            }).appendTo("." + taskStatusArray[h]);
+            $("<div>").attr({
+                id: "container" + h,
+                class: "panel-body box-container"
+            }).appendTo("." + taskStatusArray[h]);
+        } else if (taskStatusArray[h] === "closed") {
+            // console.log(taskStatusArray[h]);
+            $("<div>").attr({
+                class: "card-body " + taskStatusArray[h],
+            }).appendTo("#container" + h + "Card");
+            $("<h5>Completed Tasks</p>").attr({
+                class: "card-title",
+            }).appendTo("." + taskStatusArray[h]);
+            $("<p>These tasks have been resolved</p>").attr({
+                class: "card-text",
+            }).appendTo("." + taskStatusArray[h]);
+            $("<div>").attr({
+                id: "container2",
+                class: "panel-body box-container"
+            }).appendTo("." + taskStatusArray[h])
+        }
+    }
+}
+
+var buttonForTestingCreater = () => {
+    for (var q = 1; q <= 8; q++) {
+        if (q <= 4) {
+            $("<div>Item " + [q] + "</div>").attr({
+                itemid: "itm-" + [q],
+                class: "btn btn-danger box-item",
+            }).appendTo("#container1")
+        } else {
+            $("<div>Item " + [q] + "</div>").attr({
+                itemid: "itm-" + [q],
+                class: "btn btn-danger box-item",
+            }).appendTo("#container2");
+        }
+    }
+}
+
+var taskSummaryAndBreadCrumbCreater = () => {
+    $("<div>").attr({
+        itemid: "taskSummary",
+        class: "text-center taskList",
+    }).appendTo("#everything-container");
+    $("<ol>").attr({
+        class: "breadcrumb pagination-centered"
+    }).appendTo(".taskList");
+    $("<li>Create New Task Below</li>").attr({
+        id: "zoneSummary",
+        class: "breadcrumb-item active pagination-centered",
+        "aria-current": "page",
+    }).appendTo(".breadcrumb");
+}
+
+var newTaskButtonOnHTMLNoMySQLFunctionalityCreater = () => {
+    $("<div>").attr({
+        id: "newTaskDiv",
+        class: "center btn-group dropright",
+    }).appendTo("#everything-container");
+    $("<button>New Task</button>").attr({
+        type: "button",
+        id: "newTask",
+        class: "center btn btn-secondary dropdown-toggle",
+        "data-toggle": "dropdown",
+        "aria-haspopup": "true",
+        "aria-expanded": "false",
+    }).appendTo("#newTaskDiv")
+};
+
+var cancelTaskButtonCreator = () => {
+    $("<div>").attr({
+        id: "cancelTaskDiv",
+        class: "center btn-group dropright",
+    }).appendTo("#everything-container");
+    $("<button>Restart Task Creation</button>").attr({
+        type: "button",
+        id: "cancelTask",
+        class: "center btn btn-secondary dropdown-toggle",
+        "data-toggle": "dropdown",
+        "aria-haspopup": "true",
+        "aria-expanded": "false",
+    }).appendTo("#newTaskDiv")
+};
+
+var buttonContainerDivForLoopCreater = () => {
+    var divCreaterArray = ["zoneDrop", "departmentDrop", "roomDrop", "problemDrop", "severityDrop", "note"]
+    for (var p = 0; p < divCreaterArray.length; p++) {
+        $("<div>").attr({
+            id: "buttonContainerContainer",
+            class: "btn-group " + [p],
+            // "x-placement": "right-start"
+        }).appendTo("#everything-container");
+        $("<div>").attr({
+            // class: "",
+            id: divCreaterArray[p]
+        }).appendTo("." + [p])
+    }
+}
+
+var footerCreater = () => {
+    $("<footer>").appendTo("body");
+    $("<span>Copyright &copy; 2019 | **github repo link goes here **</span>").attr({
+        id: "footer-info"
+    }).appendTo("footer")
+}
+
+var createButton = (value, area) => {
+    if (!area) {
+        area = "#roomDrop";
+    }
+    $('<input>').attr({
+        type: 'button',
+        id: value.toLowerCase(),
+        value: value,
+        // class: ""
+    }).appendTo(area);
+}
+
+var createSeverityDropDownMenu = function () {
+    for (var i = 0; i <= 5; i++) {
+        $('<input>').attr({
+            type: "button",
+            id: i,
+            value: i,
+            class: "text-center"
+        }).appendTo("#severityDrop")
+    }
+}
--- public/js/script.js
+++ public/js/script.js
@@ -1,107 +1,12 @@
-
 $(window).on("load", function () {
-    console.log("hiding area and notes");
-    $("#notes").hide();
-});
-
-//STARTS WING DROPDOWN MENU & REPLACES WITH AREA DROPDOWN MENU
-$("#eastWing").on("click", function () {
-    $("#wing").text("East Wing / ");
-    $("#areaDrop").show();
-    $("#wingDrop").replaceWith($("#areaDrop"));
-    area();
-})
-$("#westWing").on("click", function () {
-    $("#wing").text("West Wing / ");
-    $("#areaDrop").show();
-    $("#wingDrop").replaceWith($("#areaDrop"));
-    area();
-})
-
-//FINISHES AREA AND STARTS THE ISSUE DROPDOWN MENU
-function area() {
-    $("#bathroom").on("click", function () {
-        $("#wing").append("Bathroom / ");
-        $("#issueDrop").show();
-        $("#areaDrop").replaceWith($("#issueDrop"));
-        issue();
-    })
-    $("#personalOffice").on("click", function () {
-        $("#wing").append("Personal Office / ");
-        $("#issueDrop").show();
-        $("#areaDrop").replaceWith($("#issueDrop"));
-        issue();
-    })
-    $("#cubicleArea").on("click", function () {
-        $("#wing").append("Cubicle Area / ");
-        $("#issueDrop").show();
-        $("#areaDrop").replaceWith($("#issueDrop"));
-        issue();
-    })
-    $("#lobbyOrReception").on("click", function () {
-        $("#wing").append("Lobby or Reception / ");
-        $("#issueDrop").show();
-        $("#areaDrop").replaceWith($("#issueDrop"));
-        issue();
-    })
-}
-
-//FINISHES ISSUE AND BEGINS THE NOTES INPUT FIELD
-function issue() {
-    $("#HVAC").on("click", function () {
-        $("#wing").append("HVAC / ");
-        $("#notes").show();
-        $("#issueDrop").replaceWith($("#submitNotes"));
-        notes();
-    })
-    $("#plumbing").on("click", function () {
-        $("#wing").append("Plumbing / ");
-        $("#notes").show();
-        $("#issueDrop").replaceWith($("#submitNotes"));
-        notes();
-    })
-    $("#electrical").on("click", function () {
-        $("#wing").append("Electrical / ");
-        $("#notes").show();
-        $("#issueDrop").replaceWith($("#submitNotes"));
-        notes();
-    });
-};
-
-//SUBMITS NOTES AND CREATES ISSUE notes DROPDWON
-function notes() {
-    $("#submitNotes").on("click", function () {
-    $("#wing").append(($("#issueNotes").val()) + " / " );
-    $("#notes").hide();  
-    $("#issueDrop").replaceWith($("#severityDrop"));
-    severity();
-})
-};
-
-    function severity(){
-        console.log("submitted notes");
-        $(".dropdown-menu").on("click", function (){
-        $("#wing").append($(".dropdown-item").val())
+// $(document).ready(function () {
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
+    // console.log(page);
+    htmlBaseDivsCreater();
+    if (page === "index.html") {
+        $("#newTask").on("click", function () {
+            createNameFormAndClickEvent();
         })
     }
-
-//NEW TASK OBJECT GATHERING DATA FROM EACH SELECTED OPTION
-var newTask = {
-    wing: [
-        parseInt($("#eastWing").val()),
-        parseInt($("#westWing").val())
-    ],
-    area: [
-        parseInt($("#bathroom").val()),
-        parseInt($("#office").val()),
-        parseInt($("#lobby").val())
-    ],
-    issue: [
-        parseInt($("#HVAC").val()),
-        parseInt($("#plumbing").val()),
-        parseInt($("#electrical").val())
-    ],
-    notes: [
-        $("#issueNotes").val()
-    ]
-};
+});
